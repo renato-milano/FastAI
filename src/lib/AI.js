9050b5f25e9b6d7943ce8f4b6cb3edf9804f120f
renato_milano <renatomilanodev@gmail.com>
2025-02-15 17:55:02 +0100

    Added Tools Feature for Agentic Function Call

--- src/lib/AI.js
+++ src/lib/AI.js
@@ -1,10 +1,12 @@
 import Groq from 'groq-sdk';
+import { tools,call_tool } from './tools';
 
 const client = new Groq({ apiKey: 'INSERT YOU API-KEY', dangerouslyAllowBrowser: true });
 
 // The model to use for the completion, change it based on Groq models.
 const modelTest = "llama3-70b-8192"
 const model = "llama-3.3-70b-versatile"
+const DeepSeek="deepseek-r1-distill-llama-70b"
 
 // The result of the completion.
 export async function message(conversation) {
@@ -17,35 +19,100 @@ export async function message(conversation) {
     }
 }
 
-// The stream of the completion from GROQ documentation.
+
 export async function getGroqChatStream(conversation) {
+    try {
+    // PRIMA CHIAMATA PER VERIFICARE L'USO DI TOOL
+    let response = await client.chat.completions.create({
+        model: DeepSeek,
+        messages: conversation,
+        stream: false,
+        tools: tools,
+        tool_choice: "auto",
+        max_completion_tokens: 2096,
+        reasoning_format: "parsed"
+    });
+    
+    let responseMessage = response.choices[0].message;
+    let toolCalls = responseMessage.tool_calls;
+
+    // SE NON CI SONO TOOL CHIAMATI IN TOOL_CALLS MA LA RISPOSTA 
+    // CONTIENTE LA CHIAMATA COME MESSAGGIO, ALLORA FORZA L'USO DI TOOL 
+    if(!toolCalls){
+        if(responseMessage.content.includes("<tool_call>")){
+        console.log("Chiamata forzata!")
+            response = await client.chat.completions.create({
+                model: DeepSeek,
+                messages: conversation,
+                stream: false,
+                tools: tools,
+                tool_choice: "required", // FORZA LA CHIAMATA
+                max_completion_tokens: 1000,
+            });
+        }
+    }
+    
+    responseMessage = response.choices[0].message;
+    toolCalls = responseMessage.tool_calls;
+
+    // FETCH TOOL CHIAMATI, MA NON TUTTI, ESEGUE I TOOL CHIAMATI
+    if (toolCalls &&toolCalls.length!=tools.length) {
+        
+        console.log("Ragionamento: ",responseMessage.reasoning);
+        delete responseMessage.reasoning
+        conversation.push(responseMessage);
+
+        for (const toolCall of toolCalls) {
+            const functionName = toolCall.function.name;
+            const functionArgs = JSON.parse(toolCall.function.arguments);
+            const functionResponse = call_tool(functionName,functionArgs);
+            conversation.push({
+                tool_call_id: toolCall.id,
+                role: "tool",
+                name: functionName,
+                content: await functionResponse,
+            });
+        }
+    }
+
+    // RISPOSTA FINALE
     return client.chat.completions.create({
         //
         // Required parameters
         //
         messages: conversation,
-
-        // The language model which will generate the completion.
-        model: modelTest,
+        model: ModelTest,
 
         //
         // Optional parameters
         //
-
-        // Controls randomness: lowering results in less random completions.
-        // As the temperature approaches zero, the model will become deterministic
-        // and repetitive.
         temperature: 0.5,
-
-        // The maximum number of tokens to generate. Requests can use up to
-        // 2048 tokens shared between prompt and completion.
         max_completion_tokens: 1024,
-
-        // Controls diversity via nucleus sampling: 0.5 means half of all
-        // likelihood-weighted options are considered.
         top_p: 1,
-
-        // If set, partial message deltas will be sent.
         stream: true,
     });
+    
+} catch (error) {
+
+        conversation.push({
+            role: "system",
+            content: "Errore: Qualcosa è andato storto, riprova più tardi.",
+        });
+        return client.chat.completions.create({
+            //
+            // Required parameters
+            //
+            messages: conversation,
+            model: ModelTest,
+    
+            //
+            // Optional parameters
+            //
+            temperature: 0.5,
+            max_completion_tokens: 1024,
+            top_p: 1,
+            stream: true,
+        });
+        
+}
 }
---
+++ src/lib/tools.js
@@ -0,0 +1,37 @@
+
+export const tools = [
+    {
+        type: "function",
+        function: {
+            name: "nome_funzione",
+            description: "Descrizione tool, serve a far capire all'AI a cosa serve cosi che sappia quando usarla",
+            parameters: {
+                type: "object",
+                properties: {
+                    parametro1: { type: "string" },
+                    parametro2: { type: "string" }
+                },
+                required: ["parametro1", "parametro2"],
+                additionalProperties: false
+            },
+            strict: true
+        }
+    }
+];
+
+
+// FUNZIONE CHE CHIAMA IL TOOL SELZIONATO DA AI
+export const call_tool = async (tool, args) => {
+    switch (tool) {
+    case "nome_funzione":
+            return await nome_funzione(args);
+    default:
+        return "Errore: Tool non presente"
+        break;
+  }
+};
+
+async function nome_funzione(args) {
+    const orderId = args.parametro1;
+    // FA QUALCOSA, RITORNA UNA STRINGA DI RISPOSTA
+}
